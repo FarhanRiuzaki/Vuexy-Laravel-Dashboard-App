8a6040dfae8c626cddd45741e8f709534e04c90e
Faiq Najib <faiq.najib@gmail.com>
2021-08-05 14:46:40 +0700

    update [Models] Page and Component
    - add UUID in ~/Models/Page
    - add UUID in ~/Models/Component

--- app/Models/Component.php
+++ app/Models/Component.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use App\Traits\UUID;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Component extends Model
 {
-    use HasFactory;
+    use HasFactory, UUID;
+
+    protected $primaryKey   = 'id';
+    public $incrementing    = false;
 
     protected $guarded = [];
 
--- app/Models/Page.php
+++ app/Models/Page.php
@@ -3,14 +3,18 @@
 namespace App\Models;
 
 use App\Traits\RecordSignature;
+use App\Traits\UUID;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Page extends Model
 {
-    use HasFactory, RecordSignature;
+    use HasFactory, UUID;
 
-    protected $guarded = [];
+    protected $primaryKey   = 'id';
+    public $incrementing    = false;
+
+    protected $guarded  = [];
 
     public function component()
     {
