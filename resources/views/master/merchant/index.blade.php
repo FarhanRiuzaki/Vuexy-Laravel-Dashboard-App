f12051b96c5ff0b0f1ab4a2ffc6590cd2510f661
Faiq Najib <faiq.najib@gmail.com>
2021-08-06 15:13:31 +0700

    add datatable responsive property

--- resources/views/master/merchant/index.blade.php
+++ resources/views/master/merchant/index.blade.php
@@ -101,6 +101,7 @@
             var dt_basic = dt_basic_table.DataTable({
                 processing: true,
                 serverSide: true,
+                responsive: true,
                 ajax: {
                     url: url,
                     type: 'GET',
