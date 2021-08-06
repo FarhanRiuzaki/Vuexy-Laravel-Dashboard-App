a2994980e166d72deffe9323936628da48a2955c
Faiq Najib <faiq.najib@gmail.com>
2021-08-06 15:41:07 +0700

    add Master Component Template
    - create '~/Master/ComponentTemplateController.php'
    - create Component's view files in '~/master/component/'

---
+++ resources/views/master/component/create.blade.php
@@ -0,0 +1,158 @@
+{{-- Extends layout --}}
+@extends('layouts/contentLayoutMaster')
+
+@section('title', $page_title) 
+
+@section('vendor-style')
+  {{-- vendor css files --}}
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/dataTables.bootstrap4.min.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/responsive.bootstrap4.min.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/buttons.bootstrap4.min.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/rowGroup.bootstrap4.min.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/pickers/flatpickr/flatpickr.min.css')) }}">
+@endsection
+
+@section('content')
+<style>
+
+</style>
+<div class="row">
+    @php
+        $class_offset = 'offset-2';
+    @endphp
+    {{-- @can('permission.create') --}}
+        @php
+            $class_offset = '';
+        @endphp
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="card">
+                {!! Form::open(array('route' => $route . '.store','method'=>'POST','id' => 'MyForm')) !!}
+                {{-- Header --}}
+                <div class="card-header border-bottom p-1 d-flex">
+                    <div class="head-label">
+                        <h4 class="mb-0">Add Component</h4>
+                    </div>
+                    <div>
+                        <button class="btn btn-primary data-submit mr-1">Submit All</button>
+                        <a href="{{ route($route. '.index') }}" class="btn font-weight-bold btn-warning">Cancel</a>
+                    </div>
+                </div>
+                {{-- Body --}}
+                <div class="card-body pt-2">
+                    <div class="row">
+                        <div class="col-xs-4 col-sm-4 col-md-6 col-lg-4">
+                            {{ Form::inputSelect('Page: ', 'page_id', $page, null, ['required']) }}
+                        </div>
+                        <div class="col-xs-4 col-sm-4 col-md-6 col-lg-4">
+                            {{ Form::inputText('API name: ', 'api_name', null, null, ['placeholder' => 'API name', 'required']) }}
+                        </div>
+                        <div class="col-xs-4 col-sm-4 col-md-6 col-lg-4">
+                            {{ Form::inputText('API type: ', 'api_type', null, null, ['placeholder' => 'API Type', 'required']) }}
+                        </div>
+
+                        <div class="col-xs-4 col-sm-4 col-md-6 col-lg-4">
+                            {{ Form::inputText('Type: ', 'type', null, null, ['placeholder' => 'Type', 'required'])}}
+                        </div>
+                        <div class="col-xs-4 col-sm-4 col-md-6 col-lg-4">
+                            {{ Form::inputText('Type Description: ', 'type_desc', null, null, ['placeholder' => 'Type description', 'required'])}}
+                        </div>
+                        <div class="col-xs-4 col-sm-4 col-md-6 col-lg-4">
+                            {{ Form::inputText('Header: ', 'header', null, null, ['placeholder' => 'Header', 'required'])}}
+                        </div>
+
+                        <div class="col-xs-4 col-sm-4 col-md-6 col-lg-4">
+                            {{ Form::inputText('Column Size: ', 'column_size', null, null, ['placeholder' => 'Column size', 'required'])}}
+                        </div>
+                        <div class="col-xs-4 col-sm-4 col-md-6 col-lg-4">
+                            {{ Form::inputText('Action: ', 'action', null, null, ['placeholder' => 'Action', 'required'])}}
+                        </div>
+                        <div class="col-xs-4 col-sm-4 col-md-6 col-lg-4">
+                            {{ Form::inputNumber('Sequence: ', 'sequence', null, null, ['placeholder' => 'Sequence', 'required'])}}
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <div class="card-header border-bottom p-1">
+                    <div class="head-label">
+                        <h4 class="mb-0">Add Component Parameter API</h4>
+                    </div>
+                </div>
+                {{-- Body --}}
+                <div class="card-body pt-2">
+                    <div class="table-responsive">
+                        <table class="table table-bordered tableParam">
+                            <thead>
+                                <tr class="text-center">
+                                    <th width='5px' class="font-medium-1">No</th>
+                                    <th width='45%' class="font-medium-1">Name</th>
+                                    <th width='45%' class="font-medium-1">Value</th>
+                                    <th width='5px'><button type="button" class="btn btn-success btn-icon addParam">
+                                            <i data-feather="plus"></i>
+                                    </button></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+    {{-- @endcan --}}
+
+</div>
+@endsection
+
+@push('vendor-script')
+{{-- vendor files --}}
+  <script src="{{ asset(mix('vendors/js/tables/datatable/jquery.dataTables.min.js')) }}"></script>
+  <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.bootstrap4.min.js')) }}"></script>
+  <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.responsive.min.js')) }}"></script>
+  <script src="{{ asset(mix('vendors/js/tables/datatable/responsive.bootstrap4.js')) }}"></script>
+  <script src="{{ asset(mix('vendors/js/tables/datatable/jszip.min.js')) }}"></script>
+@endpush
+
+@push('page-script')
+<script>
+    // add component parameter api
+    no  = 0;
+    $('body').on('click', '.addParam', function () {
+        no++;
+        html = '<tr>'
+            + "<td class='number text-center'>"
+                + no
+            +"</td>"
+            + "<td>"
+                + "<input type='text' data-name='name' name='component_parameter_api["+no+"][name]' class='form-control'>"
+            +"</td>"
+            + "<td>"
+                + "<input type='text' data-name='value' name='component_parameter_api["+no+"][value]' class='form-control'>"
+            +"</td>"
+            + "<td class='text-center'>"
+                + '<button type="button" class="btn btn-danger btn-icon dellParam"><i data-feather="plus"></i></button>'
+            + "</td>";
+        $('.tableParam tbody').append(html);
+    });
+
+    // delete component parameter api
+    $('body').on('click', '.dellParam', function () {
+        var tr = $(this).closest("tr");
+            tr.remove();
+
+            $.each($(".tableParam tbody tr:not(.tr-input)"),function(e,item){
+                //ganti nomor per TR
+                var no = e*1 +1;
+                $(this).find(".number").html(no);
+
+                //NEANGAN INPUTAN PER TR
+                $.each($(this).find("input"),function(s,f){
+                    var dataName = $(this).data("name");
+                    $(this).attr("name","component_parameter_api["+no+"]["+dataName+"]");
+                })
+            })
+            no = no - 1;
+    });
+</script>
+@endpush
---
+++ resources/views/master/component/index.blade.php
@@ -0,0 +1,112 @@
+{{-- Extends layout --}}
+@extends('layouts/contentLayoutMaster')
+
+@section('title', $page_title) 
+
+@section('vendor-style')
+  {{-- vendor css files --}}
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/dataTables.bootstrap4.min.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/responsive.bootstrap4.min.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/buttons.bootstrap4.min.css')) }}">
+@endsection
+
+@section('content')
+<style>
+
+</style>
+<div class="row">
+    @php
+        $class_offset = 'offset-2';
+    @endphp
+    {{-- @can('permission.create') --}}
+        @php
+            $class_offset = '';
+        @endphp
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="card card-custom">
+                {{-- Header --}}
+                <div class="card-header border-bottom p-1">
+                    <div class="head-label">
+                        <h3 class="mb-0">List Component</h3>
+                    </div>
+                    <a href="{{ route($route. '.create') }}" class="btn font-weight-bold btn-primary">Add New Component</a>
+                </div>
+                {{-- Body --}}
+                <div class="card-body table-responsive" >
+                    <table class="table table-bordered yajra-datatable">
+                        <thead>
+                            <tr class="text-center">
+                                <th>No</th>
+                                <th>API Name</th>
+                                <th>Type</th>
+                                <th>Header</th>
+                                <th>Created At</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+    {{-- @endcan --}}
+
+</div>
+@endsection
+
+@push('vendor-script')
+{{-- vendor files --}}
+  <script src="{{ asset(mix('vendors/js/tables/datatable/jquery.dataTables.min.js')) }}"></script>
+  <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.bootstrap4.min.js')) }}"></script>
+  <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.responsive.min.js')) }}"></script>
+  <script src="{{ asset(mix('vendors/js/tables/datatable/responsive.bootstrap4.js')) }}"></script>
+  <script src="{{ asset(mix('vendors/js/tables/datatable/jszip.min.js')) }}"></script>
+@endpush
+
+@push('page-script')
+<script>
+    $(function (){
+        'use strict';
+        var dt_basic_table = $('.yajra-datatable');
+        var type    = '';   // def type
+        var url     = "{{ route($route . '.datatable') }}";
+        // DataTable with buttons
+        // --------------------------------------------------------------------
+        if (dt_basic_table.length) {
+            var dt_basic = dt_basic_table.DataTable({
+                responsive: true,
+                processing: true,
+                serverSide: true,
+                ajax: { url: url },
+                columns: [
+                    {
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex',
+                        orderable: false,
+                        searchable: false
+                    },
+                    {data: 'api_name', name: 'api_name'},
+                    {data: 'type', name: 'type'},
+                    {data: 'header', name: 'header'},
+                    {data: 'created_at', name: 'created_at'},
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false
+                    },
+                ],
+                "columnDefs": [
+                    { className: "text-center", "targets": [ 0, 5] }
+                ],
+                "order": [[ 4, "desc" ]],
+                scrollY: "400px",
+                scrollCollapse: true,
+                paging: false,
+                dom: '<"d-flex justify-content-between align-items-center mx-0 row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>t<"d-flex justify-content-between mx-0 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
+            }).on( 'draw', function () {
+                $('[data-toggle="tooltip"]').tooltip();
+            });;
+        }
+    });
+</script>
+@endpush
