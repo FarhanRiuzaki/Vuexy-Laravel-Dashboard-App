5d4313df224b7ca16e54199c352920477cb4b3fe
farhanriuzaki <riuzakif@gmail.com>
2021-08-09 11:47:12 +0700

    update [user] migration

--- database/migrations/2021_07_21_025153_create_audits_table.php
+++ database/migrations/2021_07_21_025153_create_audits_table.php
@@ -14,7 +14,7 @@ class CreateAuditsTable extends Migration
     public function up()
     {
         Schema::create('audits', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->id();
             $table->string('user_type')->nullable();
             $table->uuid('user_id')->nullable();
             $table->string('event');
